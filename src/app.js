e203346c5282252174ade795621cd70992bdadb8
Benjamin Hernandez Morales <147661243+panc4ked@users.noreply.github.com>
2023-12-13 00:04:54 +0000

    avance random card generator

--- src/app.js
+++ src/app.js
@@ -1,11 +1,4 @@
-/* eslint-disable */
 import "bootstrap";
 import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+/* let arr = [A, 2, 3, 4, 5, 6, 7, 8, 9, 10, J, Q, K]; */
