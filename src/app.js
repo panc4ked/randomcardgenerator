944940adcfc48ed3e3b55f6c13745638478ca937
Benjamin Hernandez Morales <147661243+panc4ked@users.noreply.github.com>
2023-12-13 00:48:32 +0000

    random card generator listo

--- src/app.js
+++ src/app.js
@@ -1,4 +1,34 @@
 import "bootstrap";
 import "./style.css";
 
-/* let arr = [A, 2, 3, 4, 5, 6, 7, 8, 9, 10, J, Q, K]; */
+window.onload = () => {
+  document.querySelector(".card").classList.add(randomizerSuit());
+  document.querySelector(".card").innerHTML = randomizerNumber();
+};
+
+let randomizerNumber = () => {
+  let number = [
+    "A",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "10",
+    "J",
+    "Q",
+    "K"
+  ];
+  let indexnumber = Math.floor(Math.random() * number.length);
+  return number[indexnumber];
+};
+
+let randomizerSuit = () => {
+  let suits = ["diamond", "spade", "heart", "club"];
+  let indexsuit = Math.floor(Math.random() * suits.length);
+
+  return suits[indexsuit];
+};
